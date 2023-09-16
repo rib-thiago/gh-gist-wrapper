70212acc8d1d09ba6fc131b0f0fbd04e9d9cd84f
Thiago Ribeiro <mackandalls@gmail.com>
2023-09-16 13:51:03 -0300

    Versão inicial de gist_manager.sh com comandos do gh

---
+++ gist_manager.sh
@@ -0,0 +1,60 @@
+#!/usr/bin/env bash
+################################################################################
+
+#
+# Script:
+# gist_manager.sh
+
+#
+# Description:
+# Script destina-se a ser um wrapper para o comando `gh gist`
+
+#
+# Author: Thiago Ribeiro
+# Date: 2023-09-16
+# Version: 0.0.1
+
+#
+# Usage:
+# To run this script, type: ./gist_manager.sh
+
+#
+# Dependencies:
+# - gh
+
+#
+# Change Log:
+# - None.
+
+#
+# Version   Date          Author          Description
+# -------   ----          ------          -----------
+# 0.0.1     [2023-09-16]  Thiago Ribeiro  Initial release.
+
+
+#
+# TODO: None.
+
+#
+# FIXME: None.
+
+################################################################################
+
+
+# display de menu de ajuda
+## Função que extrai o id de um gist? 
+# diretório para salvar gists
+# download de gists em pasta oculta
+# incluir pasta oculta no .gitignore
+
+
+
+# Available commands
+CLONE='gh gist clone'
+CREATE='gh gist create'
+DELETE='gh gist delete'
+EDIT='gh gist edit'
+LIST='gh gist list'
+RENAME='gh gist rename'
+VIEW='gh gist view'
+
