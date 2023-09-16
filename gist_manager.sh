02f0453af4d984f4f702160121f4362f327479de
Thiago Ribeiro <mackandalls@gmail.com>
2023-09-16 17:15:21 -0300

    Adicionando funcionalidades de criar e clonar gists

--- gist_manager.sh
+++ gist_manager.sh
@@ -40,13 +40,16 @@
 
 ################################################################################
 
-
-# display de menu de ajuda
-## Função que extrai o id de um gist? 
-# diretório para salvar gists
-# download de gists em pasta oculta
-# incluir pasta oculta no .gitignore
-
+## Paleta de Cores
+BLACK="\033[30m"
+RED="\033[31m"
+GREEN="\033[32m"
+YELLOW="\033[33m"
+BLUE="\033[34m"
+MAGENTA="\033[35m"
+CYAN="\033[36m"
+WHITE="\033[37m"
+RESET="\033[0m"
 
 # Available commands
 CLONE='gh gist clone'
@@ -54,31 +57,31 @@ CREATE='gh gist create'
 DELETE='gh gist delete'
 LIST='gh gist list --secret'
 VIEW='gh gist view'
+EDIT='gh gist edit'
 
+# Definir um array associativo para mapear nomes de Gists para IDs
+declare -A gist_map
 
-# count_columns_with_awk() {
-#     local output="$1"
-#     local number_of_columns=$(echo "$output" | awk 'NR==0 {print NF}')
-#     echo "Número de colunas: $number_of_columns"
-# }
+# Função para listar os Gists
+list_gists() {
+    local output="$($LIST)"
+    create_dictionary "$output"  # Chama a função para criar o dicionário
+    echo -e "${MAGENTA}Gists Catalogados: ${RESET}"
+    echo -e ""
+    
+    # Loop sobre as linhas e exiba as strings
+    for line_str in "${lines[@]}"; do
+        echo -e "${CYAN}$line_str${RESET}"
+    done
+}
 
-# get_first_field_with_awk() {
-#     local output="$1"
-#     local first_field=$(echo "$output" | awk 'NR==0 {print $1}')
-#     echo "$first_field"
-# }
-
-
-
-# output=$($LIST --secret)
-
-
-# count_columns_with_awk "$output"
-# get_first_field_with_awk "$output"
-
+# Função para criar um dicionário de Gists a partir da saída do comando
 create_dictionary() {
     local output="$1"
-    local lines=()
+    lines=()
+
+    # Limpar o gist_map para evitar IDs antigos
+    gist_map=()
 
     # Ler cada linha da saída e adicionar ao array 'lines'
     while IFS= read -r line; do
@@ -87,49 +90,117 @@ create_dictionary() {
         # Verificar se ambos os campos têm conteúdo antes de adicionar ao dicionário
         if [ -n "$first_field" ] && [ -n "$second_field" ]; then
             lines+=("$first_field : $second_field")
+            
+            # Adicionar o ID do Gist ao gist_map
+            gist_map["$second_field"]=$first_field
         fi
     done <<< "$output"
-
-    echo -e ""
-    # Loop sobre as linhas e exiba as strings
-    for line_str in "${lines[@]}"; do
-        echo "$line_str"
-    done
 }
-cmd=$($LIST)
-create_dictionary "$cmd"
+
+
+# Função para criar um novo Gist
+create_gist() {
+    echo "Como você deseja criar o Gist?"
+    echo "1. A partir de um arquivo existente"
+    echo "2. Digitar o conteúdo agora"
+    read -p "Escolha uma opção: " create_option
+
+    case $create_option in
+        1)
+            echo "Digite o caminho completo do arquivo que você deseja criar o Gist:"
+            read file_path
+            filename=$(basename "$file_path")
+            echo "Digite uma descrição para o Gist:"
+            read gist_description
+            gist_url="$($CREATE -d "$gist_description" -f "$filename" < "$file_path")"
+            gist_id=$(basename "$gist_url")
+            ;;
+        2)
+            echo "Digite o nome do arquivo:"
+            read filename
+            echo "Digite o conteúdo do arquivo (pressione Ctrl+D para encerrar):"
+            content=$(cat)
+            echo "Digite uma descrição para o Gist:"
+            read gist_description
+            gist_url="$($CREATE -d "$gist_description" -f "$filename" <<< "$content")"
+            gist_id=$(basename "$gist_url")
+            ;;
+        *)
+            echo "Opção inválida."
+            return
+            ;;
+    esac
+
+    echo "Gist criado com sucesso. ID: $gist_id"
+
+}
+
+
+
+
+# Função para clonar um Gist
+clone_gist() {
+    echo "Escolha um Gist para clonar:"
+    
+    # Criar uma lista de opções formatadas como uma tabela
+    options_list=""
+    for name in "${!gist_map[@]}"; do
+        options_list+="$(printf "%-30s" "$name") : ${gist_map[$name]}\n"
+    done
+    
+    # Exibir a lista formatada usando 'column'
+    echo -e "$options_list" | column -t -s ':'
+    read -p "Digite o nome do Gist que deseja clonar: " gist_name
+    gist_id="${gist_map[$gist_name]}"
+    if [ -n "$gist_id" ]; then
+        $CLONE "$gist_id"
+        echo "Gist clonado com sucesso."
+    else
+        echo "Gist não encontrado."
+    fi
+}
+
 
 
 # Inicio
-create_dictionary
-echo -e ""
-echo "Selecione uma ação: "
-echo -e ""
-echo "1. Listar Gists"
-echo "2. Visualizar Gists"
-echo "3. Editar Gists"
-echo "4. Clonar Gists"
-echo "5. Deletar Gists"
-echo -e "\n"
-read -p  "Qual a Sua opção: " option
 
-case $option in
-    1)
-        echo "1. Listar Gists"
-    ;;
-    2)
-        echo "2. Visualizar Gists"
-    ;;
-    3)
-        echo "3. Editar Gists"
-    ;;
-    4)
-        echo "4. Clonar Gists"
-    ;;
-    5)
-        echo "5. Deletar Gists"
-    ;;
-    *)
-        echo "A escolha não é válida."
-    ;;
-esac
+# Menu principal
+while true; do
+    echo "Selecione uma ação:"
+    echo "1. Listar Gists"
+    echo "2. Criar Gist"
+    echo "3. Clonar Gist"
+    echo "4. Visualizar Gist"
+    echo "5. Editar Gist"
+    echo "6. Deletar Gist"
+    echo "7. Sair"
+    read -p "Qual é a sua escolha: " option
+
+    case $option in
+        1)
+            list_gists
+            ;;
+        2)
+            create_gist
+            ;;
+        3)
+            clone_gist
+            ;;
+        4)
+            # Implementar visualização de Gist
+            ;;
+        5)
+            # Implementar edição de Gist
+            ;;
+        6)
+            # Implementar exclusão de Gist
+            ;;
+        7)
+            echo "Saindo..."
+            exit 0
+            ;;
+        *)
+            echo "Opção inválida. Tente novamente."
+            ;;
+    esac
+done
